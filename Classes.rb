1a50759a45126cbc6072afa6859211c8df5cd3bd
AlexSidorov88 <105877232+AlexSidorov88@users.noreply.github.com>
2023-01-14 21:42:48 +0100

    Add files via upload

---
+++ Classes.rb
@@ -0,0 +1,19 @@
+class Manager
+   def self.say_hi
+      puts 'Hi'
+   end
+end
+class Hipster
+   def self.say_hi
+      puts 'Hiii,yo'
+   end
+end
+class JessiePinkman
+   def self.say_hi
+      puts 'Hi,bitch'
+   end
+end
+
+Manager.say_hi
+Hipster.say_hi
+JessiePinkman.say_hi
