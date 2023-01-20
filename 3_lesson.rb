2634bdb81604dd588e63c3ebc9ce57ade7d7e70a
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-01-20 12:41:21 +0100

    Add files via upload
    each

---
+++ 3_lesson.rb
@@ -0,0 +1,6 @@
+members = ["floyd", "arthuro", "manny", "mike"] #elements, type string
+
+members.each do |member| #Block
+   #инструкции,которые нужно сделать для каждого взятого элемента массива
+
+end
